7233504a303266d322a27f156016026a91758c80
santosraquel <raquel.dossantos1965@gmail.com>
2022-02-17 18:49:49 -0300

    test alter server port

--- lojavirtual/src/main/java/com/dev/lojavirtual/LojavirtualApplication.java
+++ lojavirtual/src/main/java/com/dev/lojavirtual/LojavirtualApplication.java
@@ -1,5 +1,7 @@
 package com.dev.lojavirtual;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LojavirtualApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LojavirtualApplication.class, args);
+		// SpringApplication.run(LojavirtualApplication.class, args);
+		SpringApplication app = new SpringApplication(LojavirtualApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+		app.run(args);
 	}
 
 }
