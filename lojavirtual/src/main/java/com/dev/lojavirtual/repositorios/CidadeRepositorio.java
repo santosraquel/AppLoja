302fb4a9ae384c46c39968cf25ac0c049b9cadcc
santosraquel <raquel.dossantos1965@gmail.com>
2022-02-22 21:23:15 -0300

    creating cities register

---
+++ lojavirtual/src/main/java/com/dev/lojavirtual/repositorios/CidadeRepositorio.java
@@ -0,0 +1,9 @@
+package com.dev.lojavirtual.repositorios;
+
+import com.dev.lojavirtual.modelos.Cidade;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CidadeRepositorio extends JpaRepository<Cidade, Long> {
+
+}
