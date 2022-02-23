89df6d937e99111752e0fe715554bcf8b1669245
santosraquel <raquel.dossantos1965@gmail.com>
2022-02-22 23:29:46 -0300

    creating product entry page

---
+++ lojavirtual/src/main/java/com/dev/lojavirtual/repositorios/EntradaItensRepositorio.java
@@ -0,0 +1,9 @@
+package com.dev.lojavirtual.repositorios;
+
+import com.dev.lojavirtual.modelos.EntradaItens;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntradaItensRepositorio extends JpaRepository<EntradaItens, Long> {
+
+}
---
+++ lojavirtual/src/main/java/com/dev/lojavirtual/repositorios/EntradaProdutoRepositorio.java
@@ -0,0 +1,9 @@
+package com.dev.lojavirtual.repositorios;
+
+import com.dev.lojavirtual.modelos.EntradaProduto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntradaProdutoRepositorio extends JpaRepository<EntradaProduto, Long> {
+
+}
